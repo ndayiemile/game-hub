f92fa6d9a256ea0dab47be0bdb3be0cee59d0082
ndayiemile <ndayishimiyeemile96@gmail.com>
2024-12-27 21:56:04 -0500

    create a generic data fetching hook

--- src/Hooks/useGames.ts
+++ src/Hooks/useGames.ts
@@ -1,7 +1,4 @@
-import apiClient from "@/services/api-client";
-import { CanceledError } from "axios";
-import { useEffect, useState } from "react";
-
+import useData from "./useData";
 export interface Platform {
   id: number;
   name: string;
@@ -14,31 +11,10 @@ export interface Game {
   parent_platforms: { platform: Platform }[];
   metacritic: number;
 }
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
+export interface Genre {
+  id: number;
+  name: string;
 }
-const useGames = () => {
-  const controller = new AbortController();
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    apiClient
-      .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => {
-        setGames(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-    return () => controller.abort();
-  }, []);
-  return { games, error, isLoading };
-};
+const useGames = () => useData<Game>("/games");
 
 export default useGames;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,7 +4,7 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding="10px" gap={10}>
@@ -15,7 +15,7 @@ const GameGrid = () => {
           </GameCardContainer>
         ))}
       {error && <p>{error}</p>}
-      <For each={games}>
+      <For each={data}>
         {(game) => (
           <GameCardContainer>
             <GameCard key={game.id} game={game} />
