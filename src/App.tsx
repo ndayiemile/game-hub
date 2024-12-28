42bb5f70f0969a87495d25587aa4b5719fe84cb6
ndayiemile <ndayishimiyeemile96@gmail.com>
2024-12-27 23:31:46 -0500

    HighLight the selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,10 @@ function App() {
           <Navbar />
         </GridItem>
         <GridItem hideBelow="lg" area="aside" paddingX={5}>
-          <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} />
+          <GenreList
+            selectedGenre={selectedGenre}
+            onSelectGenre={(genre) => setSelectedGenre(genre)}
+          />
         </GridItem>
         <GridItem area="main">
           <GameGrid selectedGenre={selectedGenre} />
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,8 +3,9 @@ import { HStack, Image, List, Spinner, Text } from "@chakra-ui/react";
 import getCroppedImageUrl from "./image-url";
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
   if (isLoading) return <Spinner />;
@@ -20,6 +21,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
             />
             <Text
               fontSize="lg"
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               css={{
                 _hover: { cursor: "pointer", textDecoration: "underline" },
               }}
