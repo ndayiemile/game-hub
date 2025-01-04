b218354a95154cac7149d2107aefe9ffaabee682
ndayiemile <ndayishimiyeemile96@gmail.com>
2025-01-03 20:50:42 -0500

    Refactoring fetch games with react query

--- src/Hooks/useGames.ts
+++ src/Hooks/useGames.ts
@@ -1,5 +1,6 @@
 import { GameQuery } from "@/App";
-import useData from "./useData";
+import apiClient, { FetchResponse } from "@/services/api-client";
+import { useQuery } from "@tanstack/react-query";
 export interface Platform {
   id: number;
   name: string;
@@ -14,17 +15,18 @@ export interface Game {
   rating_top: number;
 }
 const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platform: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      },
-    },
-    [gameQuery]
-  );
-
+  useQuery<FetchResponse<Game>, Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Game>>("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            parent_platform: gameQuery.platform?.id,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data),
+  });
 export default useGames;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
-
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
 const create = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
