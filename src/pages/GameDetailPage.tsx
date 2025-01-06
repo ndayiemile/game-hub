242dd4618d579f004cd59701bdff6c0edcbca30f
ndayiemile <ndayishimiyeemile96@gmail.com>
2025-01-06 01:20:53 -0500

    build game trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,9 +1,8 @@
-import CriticScore from "@/components/CriticScore";
-import DefinitionItem from "@/components/DefinitionItem";
 import ExpandableText from "@/components/ExpandableText";
 import GameAttributes from "@/components/GameAttributes";
+import GameTrailer from "@/components/GameTrailer";
 import useGame from "@/Hooks/useGame";
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 
 const GameDetailPage = () => {
@@ -18,6 +17,7 @@ const GameDetailPage = () => {
         {game.description_raw}
       </ExpandableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
